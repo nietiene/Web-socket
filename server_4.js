5625a20cf9a19e99e4335feeacd58a8c30a2b197
nietiene <niyomugaboetiene53@gmail.com>
2025-07-01 08:00:14 -0700

    first commit of socket io

--- server_4.js
+++ server_4.js
@@ -1,3 +1,16 @@
+const { Socket } = require("dgram");
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/room.html");
+});
+
+io.on('connection', (socket) => {
+    
+})
