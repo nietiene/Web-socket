841b86391711fe6829b462aeea8fc3ac7d8a9205
nietiene <niyomugaboetiene53@gmail.com>
2025-07-02 09:44:10 -0700

    first commit of socket io

--- Private-message-app/server_3.js
+++ Private-message-app/server_3.js
@@ -4,6 +4,7 @@ const session = require("express-session");
 const mysql = require("mysql2");
 const { Server } = require("socket.io");
 const path = require("path");
+const { Socket } = require("dgram");
 
 const app = express();
 const server = http.createServer(app);
@@ -22,3 +23,12 @@ const sessionMiddleware = session({
     resave: false,
     saveUninitialized: true
 });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(sessionMiddleware);
+app.use(express.static(__dirname));
+
+io.use((socket, next) => {
+    sessionMiddleware(socket.request, {}, next);
+});
+
