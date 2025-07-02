2208d7da1559525de51b24b4ba80a34f2ccc6f74
nietiene <niyomugaboetiene53@gmail.com>
2025-07-02 09:41:49 -0700

    first commit of socket io

---
+++ Private-message-app/server_3.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const http = require("http");
+const session = require("express-session");
+const mysql = require("mysql2");
+const { Server } = require("socket.io");
+const path = require("path");
